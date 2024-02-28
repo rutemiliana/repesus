b3bddade555638f0671ce51270f8af3660c17bd2
rutemiliana <rutemiliana13@gmail.com>
2024-02-28 17:18:51 -0300

    adicionando campos na model uders

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,13 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'dateOfBirth',
         'email',
-        'password',
+        'lattes',
+        'orcid',
+        'affiliationId',
+        'acessLevelId',
+        'password'
     ];
 
     /**
