4f75076296387d3700e02d414c72f684146a9b14
rutemiliana <rutemiliana13@gmail.com>
2024-03-31 21:18:10 -0300

    criando view para submissão de pesquisa

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/researches', function () {
+        return view('researches.create');
+    });
+
 });
 
 require __DIR__.'/auth.php';
