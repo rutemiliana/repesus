bc81a5c8ad1ec0c8e7553d2eabb83efa6449c9e7
rutemiliana <rutemiliana13@gmail.com>
2024-06-02 18:52:02 -0300

    atualizando a página principal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,7 +57,7 @@
             <div class="lg:text-center">
                 <h3 class="text-lg leading-6 font-medium text-gray-900">Nossa Equipe</h3>
                 <p class="mt-2 text-base text-gray-500">
-                    Conheça os pesquisadores que fazem tudo acontecer.
+                    Conheça a Rede de Pesquisadores do SUS - DF.
                 </p>
             </div>
 
@@ -65,42 +65,42 @@
                 <ul class="md:grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                     <li>
                         <div class="space-y-6">
-                            <img class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" src="https://via.placeholder.com/150" alt="Foto do Pesquisador 1">
+                            <img class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" src="{{asset('logofepecs.png')}}" alt="LOGO FEPECS">
                             <div class="space-y-2">
                                 <div class="text-lg leading-6 font-medium space-y-1">
-                                    <h3>José Silva</h3>
-                                    <p class="text-blue-600">Chefe de Pesquisa em Biologia</p>
+                                    <h3>FEPECS</h3>
+                                    <p class="text-blue-600">Mantenedora da Rede</p>
                                 </div>
                                 <p class="text-gray-500">
-                                    José é um bioquímico com mais de 20 anos de experiência em pesquisa genética.
+                                Fundação de Ensino e Pesquisa em Ciências da Saúde
                                 </p>
                             </div>
                         </div>
                     </li>
                     <li>
                         <div class="space-y-6">
-                            <img class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" src="https://via.placeholder.com/150" alt="Foto do Pesquisador 2">
+                            <img class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" src="{{asset('repesus.png')}}" alt="Foto RePeSUS e sob o mapa de Brasília">
                             <div class="space-y-2">
                                 <div class="text-lg leading-6 font-medium space-y-1">
-                                    <h3>Maria Fernanda</h3>
-                                    <p class="text-blue-600">Especialista em Inteligência Artificial</p>
+                                    <h3>Hospitais da Secretaria de Saúde</h3>
+                                    <p class="text-blue-600">Hospitais da SES que atendam SUS</p>
                                 </div>
                                 <p class="text-gray-500">
-                                    Maria Fernanda lidera projetos inovadores na interseção entre IA e saúde.
+                                Hospitais da Secretaria de Saúde
                                 </p>
                             </div>
                         </div>
                     </li>
                     <li>
                         <div class="space-y-6">
-                            <img class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" src="https://via.placeholder.com/150" alt="Foto do Pesquisador 3">
+                            <img class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56" src="{{asset('logoescs.png')}}" alt="Logo da ESCS">
                             <div class="space-y-2">
                                 <div class="text-lg leading-6 font-medium space-y-1">
-                                    <h3>Carlos Gomes</h3>
-                                    <p class="text-blue-600">Pesquisador Sênior em Física Quântica</p>
+                                    <h3>ESCS</h3>
+                                    <p class="text-blue-600"> Escola Superior de Ciências da Saúde</p>
                                 </div>
                                 <p class="text-gray-500">
-                                    Carlos é um físico quântico com vasta publicação em teoria das cordas e mecânica quântica.
+                                Escola Superior de Ciências da Saúde
                                 </p>
                             </div>
                         </div>
